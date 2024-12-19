476c921f2ad30726fa8b385151d1dcba41b6e3b6
jone2337 <jone2337@purdue.edu>
2024-12-19 18:18:21 -0500

    requirements

--- app.py
+++ app.py
@@ -7,9 +7,8 @@ def serve_index():
     return send_from_directory("static", "index.html")
 
 @app.route("/<path:path>")
-def serve_static():
+def serve_static(path):
     return send_from_directory("static", path)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-    
